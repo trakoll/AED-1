2733d67c62c21c6853d292f9871aa9867af87428
trakoll <90356428+trakoll@users.noreply.github.com>
2025-11-28 19:54:49 -0300

    Create 1197.c

---
+++ 1197.c
@@ -0,0 +1,13 @@
+/* Kauan Dias Caisley AED 1 Volta à Faculdade de Física */
+
+#include <stdio.h>
+
+int main() {
+    int v, t;
+
+    while (scanf("%d %d", &v, &t) != EOF) {
+        printf("%d\n", 2 * v * t); // essa foi dificil. 
+    }
+
+    return 0;
+}
