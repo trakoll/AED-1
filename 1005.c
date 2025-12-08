6adde986782834f0657e6126bb9246255d8cf98f
trakoll <90356428+trakoll@users.noreply.github.com>
2025-12-08 10:04:36 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,11 @@
+/* Kauan Dias Caisley AED 1 Problema muito complexo */
+
+#include <stdio.h>
+ 
+int main() {
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("MEDIA = %.5f\n", (a*3.5 + b*7.5) / 11.0);
+    
+    return 0;
+}
